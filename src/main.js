31405dbc7d03f29a450f30fdb29ffcc914c9e728
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-08-27 19:28:49 +0300

    fill styles.css, index.html and main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './js/refs';
+import './js/constants';
+import './js/helpers';
+import './js/storage';
+
+import './js/header';
+import './js/hero';
+import './js/artists';
+import './js/artists-additional';
+import './js/about-us';
+import './js/feedback';
+import './js/footer';
+
+import './js/artist-details-modal';
+import './js/feedback-modal';
