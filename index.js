d79db11c3c57a7e0ca407591fc6a2bb58bfd0004
ZVyacheslavV <vvvv.zkv@gmail.com>
2025-08-28 19:33:28 +0000

    Deploying to gh-pages from @ ZVyacheslavV/developers-orchestra-project@a15ae749627bc86e1a4f5d764f98d74e1b8be5d1 🚀

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import{a as c,i as l}from"./assets/vendor-Cl-WcV8j.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const a of e.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function o(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function i(t){if(t.ep)return;t.ep=!0;const e=o(t);fetch(t.href,e)}})();const u={artistsList:document.querySelector("#artists-list")},d="https://sound-wave.b.goit.study/api",f={ARTISTS:"/artists",ARTIST_BY_ID:"/artists/",ARTIST_ALBUMS_BY_ID:"/albums",GENRES:"/genres",FEEDBACKS:"/feedbacks",PRODUCTS_BY_CATEGORY:"/products/category/"},p=8,m={currentPage:1};c.defaults.baseURL=d;const g=async s=>{const{data:r}=await c.get(`${f.ARTISTS}?limit=${p}&page=${s}`);return r};function y(s){l.error({title:"Error",message:s})}async function h(){try{const{artists:s}=await g(m.currentPage);A(s)}catch{y("Failed to fetch artists")}}function A(s=[]){const r=s.map(({_id:o,strArtist:i="Unknown",strArtistThumb:t,strBiographyEN:e="",genres:a=[]})=>`
+import{a as n,i as l}from"./assets/vendor-Cl-WcV8j.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const i of e.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function a(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function r(t){if(t.ep)return;t.ep=!0;const e=a(t);fetch(t.href,e)}})();const d={artistsList:document.querySelector("#artists-list")},u="https://sound-wave.b.goit.study/api",f={ARTISTS:"/artists",ARTIST_BY_ID:"/artists/",ARTIST_ALBUMS_BY_ID:"/albums",GENRES:"/genres",FEEDBACKS:"/feedbacks",PRODUCTS_BY_CATEGORY:"/products/category/"},p=8,y={currentPage:1};n.defaults.baseURL=u;const m=async s=>{const{data:o}=await n.get(`${f.ARTISTS}?limit=${p}&page=${s}`);return o};function g(s){l.error({title:"Error",message:s})}async function S(){try{const{artists:s}=await m(y.currentPage);h(s)}catch{g("Failed to fetch artists")}}function h(s=[]){const o=s.map(({_id:a,strArtist:r="Unknown",strArtistThumb:t,strBiographyEN:e="",genres:i=[]})=>`
             <li class="artists-item">
-              <div class="artist-card" data-id="${o}">
+              <div class="artist-card" data-id="${a}">
                 <img
                   src="${t||"https://placehold.co/736x414?text=No+Image"}"
-                  alt="${i} — portrait"
+                  alt="${r} — portrait"
                   class="artist-img"
                   loading="lazy"
                   decoding="async"
                   width="736" height="414"
                 >
-                ${Array.isArray(a)&&a.length?`<ul class="artist-tags">${a.map(n=>`<li class="tag">${n}</li>`).join("")}</ul>`:""}
+                ${Array.isArray(i)&&i.length?`<ul class="artist-tags">${i.map(c=>`<li class="tag">${c}</li>`).join("")}</ul>`:""}
 
-                <h3 class="artist-name">${i}</h3>
+                <h3 class="artist-name">${r}</h3>
                 ${e?`<p class="artist-desc text-clamp-3">${e}</p>`:""}
 
-                <button class="artist-cta" type="button" aria-label="Learn more about ${i}">
+                <button class="artist-cta" type="button" aria-label="Learn more about ${r}">
                   Learn More
                 </button>
               </div>
-            </li>`).join("");u.artistsList.insertAdjacentHTML("beforeend",r)}h();
+            </li>`).join("");d.artistsList.insertAdjacentHTML("beforeend",o)}S();document.querySelectorAll(".dropdown").forEach(s=>{const o=s.querySelector(".dropdown-toggle"),a=s.querySelector(".dropdown-menu");o.addEventListener("click",()=>{a.style.display=a.style.display==="block"?"none":"block"}),a.querySelectorAll("li").forEach(r=>{r.addEventListener("click",()=>{o.textContent=r.textContent,o.dataset.value=r.dataset.value,a.style.display="none"})}),document.addEventListener("click",r=>{s.contains(r.target)||(a.style.display="none")})});
 //# sourceMappingURL=index.js.map
