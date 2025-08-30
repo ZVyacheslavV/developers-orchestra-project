52d6eb6f37bdac7b1243c092fc5bc474cf3d13f1
Yevheniia Serhieieva <y.o.serhieieva@gmail.com>
2025-08-30 13:25:04 +0300

    all

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,8 @@
 /* Refs */
 export const refs = {
   artistsList: document.querySelector('#artists-list'),
+  artistDetailsModal: document.querySelector('.artist-details-modal'),
+  artistDetailsModalCloseBtn: document.querySelector('.artist-details-modal-close-btn'), //make btn close
 };
+
+
