a04cbcd4660cd36e76132556da747fd70c0c007b
nata_sh <ang416@gmail.com>
2025-08-31 10:23:22 +0300

    Create header

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,25 @@
-/* Header */
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.getElementById('burger');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const closeMenu = document.getElementById('closeMenu');
+  const menuLinks = document.querySelectorAll('.header-mob-nav-link');
+  const overlay = document.getElementById('menuOverlay');
+
+  burger.addEventListener('click', () => {
+    mobileMenu.classList.add('active');
+  });
+
+  closeMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+  });
+
+  overlay.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+  });
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.remove('active');
+    });
+  });
+});
