5af19a79499158ceef1c7e5d7336108408cba53d
KaterynaBlinova-1995 <catalina.ova2709@gmail.com>
2025-08-31 21:00:56 +0300

    all files

--- src/js/button-up.js
+++ src/js/button-up.js
@@ -2,24 +2,29 @@ const scrollBtn = document.querySelector('.scroll-to-top');
 
 let lastScroll = window.scrollY;
 let isVisible = false;
+let scrollTimeout;
 
 window.addEventListener('scroll', () => {
-  const currentScroll = window.scrollY;
-  const scrollingUp = currentScroll < lastScroll;
-  const scrolledToBottom =
-    window.innerHeight + currentScroll >= document.body.offsetHeight;
+  clearTimeout(scrollTimeout);
 
-  const shouldShow = scrolledToBottom || (scrollingUp && currentScroll > 300);
+  scrollTimeout = setTimeout(() => {
+    const currentScroll = window.scrollY;
+    const scrollingUp = currentScroll < lastScroll - 30;
+    const scrolledToBottom =
+      window.innerHeight + currentScroll >= document.body.offsetHeight - 10;
 
-  if (shouldShow && !isVisible) {
-    scrollBtn.classList.add('visible');
-    isVisible = true;
-  } else if (!shouldShow && isVisible) {
-    scrollBtn.classList.remove('visible');
-    isVisible = false;
-  }
+    const shouldShow = scrolledToBottom || (scrollingUp && currentScroll > 350);
 
-  lastScroll = currentScroll;
+    if (shouldShow && !isVisible) {
+      scrollBtn.classList.add('visible');
+      isVisible = true;
+    } else if (!shouldShow && isVisible) {
+      scrollBtn.classList.remove('visible');
+      isVisible = false;
+    }
+
+    lastScroll = currentScroll;
+  }, 70);
 });
 
 scrollBtn.addEventListener('click', () => {
