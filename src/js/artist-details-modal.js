7b5ad1ca3e6741b9cd1f68d3593235ad5fd66d4d
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-09-04 23:02:05 +0300

    add click on card move

--- src/js/artist-details-modal.js
+++ src/js/artist-details-modal.js
@@ -1,24 +1,26 @@
 /* Artist details modal */
 
-import { getArtistAlbumsById, getArtistById } from "./artists-api";
-import { toastError } from "./helpers";
-import { refs } from "./refs";
-
+import { getArtistAlbumsById, getArtistById } from './artists-api';
+import { toastError } from './helpers';
+import { refs } from './refs';
 
 function openModal() {
-    refs.artistDetailsModalBackdrope.classList.add('modal--is-open');
-    document.body.style.overflow = 'hidden';
-    window.addEventListener('keydown', onEcsKeyPress);
-    refs.artistDetailsModalBackdrope.addEventListener('click', onBackdropClick);
+  refs.artistDetailsModalBackdrope.classList.add('modal--is-open');
+  document.body.style.overflow = 'hidden';
+  window.addEventListener('keydown', onEcsKeyPress);
+  refs.artistDetailsModalBackdrope.addEventListener('click', onBackdropClick);
 }
 
 function closeModal() {
-    refs.artistDetailsModalBackdrope.classList.remove('modal--is-open');
-    document.body.style.overflow = '';
-    window.removeEventListener('keydown', onEcsKeyPress);
-    refs.artistDetailsModalBackdrope.removeEventListener('click', onBackdropClick);
-    refs.artistDetailsModal.innerHTML = '<span class="loader is-hidden"></span>';
-    refs.artistDetailsModal.innerHTML = '';
+  refs.artistDetailsModalBackdrope.classList.remove('modal--is-open');
+  document.body.style.overflow = '';
+  window.removeEventListener('keydown', onEcsKeyPress);
+  refs.artistDetailsModalBackdrope.removeEventListener(
+    'click',
+    onBackdropClick
+  );
+  refs.artistDetailsModal.innerHTML = '<span class="loader is-hidden"></span>';
+  refs.artistDetailsModal.innerHTML = '';
 }
 
 function onEcsKeyPress(e) {
@@ -34,61 +36,59 @@ function onBackdropClick(e) {
 }
 
 function showLoader() {
-    refs.artistDetailsModal.innerHTML = '<span class="loader"></span>';
+  refs.artistDetailsModal.innerHTML = '<span class="loader"></span>';
 }
 
 function hideLoader() {
-    const loader = refs.artistDetailsModal.querySelector('.loader');
-    if (loader) loader.remove();
+  const loader = refs.artistDetailsModal.querySelector('.loader');
+  if (loader) loader.remove();
 }
-    
 
 async function showArtistDetails(artistId) {
-    
-    try {  
-        openModal();
-        showLoader();        
+  try {
+    openModal();
+    showLoader();
 
-        const artist = await getArtistById(artistId);
-        const albumsData = await getArtistAlbumsById(artistId);
-        const albums = albumsData?.albumsList || [];
-        
-        const { 
-            intFormedYear, 
-            intDiedYear, 
-            strArtist, 
-            strArtistThumb, 
-            strGender, 
-            intMembers, 
-            strCountry, 
-            strBiographyEN, 
-            genres,
-        } = artist;
-   
-        let years;
-        if (intFormedYear && intDiedYear) {
-        years = `${intFormedYear} - ${artist.intDiedYear}`;
-        } else if (intFormedYear && !intDiedYear) {
-        years = `${intFormedYear} - present`;
-        } else {
-        years = 'information missing';
-        }
+    const artist = await getArtistById(artistId);
+    const albumsData = await getArtistAlbumsById(artistId);
+    const albums = albumsData?.albumsList || [];
 
-        const formatDuration = ms => {
-            if (!ms || isNaN(ms)) return '';
-            const totalSec = Math.floor(ms / 1000);
-            const min = Math.floor(totalSec / 60);
-            const sec = String(totalSec % 60).padStart(2, '0');
-            return `${min}:${sec}`;
-        };
+    const {
+      intFormedYear,
+      intDiedYear,
+      strArtist,
+      strArtistThumb,
+      strGender,
+      intMembers,
+      strCountry,
+      strBiographyEN,
+      genres,
+    } = artist;
 
-        const imageUrl = artist.strArtistThumb 
-            ? artist.strArtistThumb 
-            : 'https://placehold.co/736x414?text=No+Image';
+    let years;
+    if (intFormedYear && intDiedYear) {
+      years = `${intFormedYear} - ${artist.intDiedYear}`;
+    } else if (intFormedYear && !intDiedYear) {
+      years = `${intFormedYear} - present`;
+    } else {
+      years = 'information missing';
+    }
 
-        const sprite = new URL('../img/icons.svg', import.meta.url).href;
+    const formatDuration = ms => {
+      if (!ms || isNaN(ms)) return '';
+      const totalSec = Math.floor(ms / 1000);
+      const min = Math.floor(totalSec / 60);
+      const sec = String(totalSec % 60).padStart(2, '0');
+      return `${min}:${sec}`;
+    };
 
-        const markup = `
+    const imageUrl = artist.strArtistThumb
+      ? artist.strArtistThumb
+      : 'https://placehold.co/736x414?text=No+Image';
+
+    const sprite = new URL('../img/icons.svg', import.meta.url).href;
+
+    const markup = `
         <div class="artist-details-modal-content">
             <button class="artist-details-modal-close-btn" type="button" aria-label="Close the detailed information window">
                 <svg class="modal-svg" width="32" height="32">
@@ -98,7 +98,9 @@ async function showArtistDetails(artistId) {
 
             <h2 class="artist-details-modal-main-title">${strArtist}</h2>
             <div class="artist-details-modal-main-block">
-                <img class="artist-details-modal-img" src="${imageUrl}" alt="${artist.name}" />
+                <img class="artist-details-modal-img" src="${imageUrl}" alt="${
+      artist.name
+    }" />
 
                 <div class="artist-details-modal-info">
                 <ul class="artist-details-modal-list">
@@ -106,18 +108,26 @@ async function showArtistDetails(artistId) {
                     <h3 class="artist-details-modal-title">Years active</h3>
                     <p  class="artist-details-modal-text">${years}</p>
                 </li>
-                ${strGender ? `
+                ${
+                  strGender
+                    ? `
                     <li class="artist-details-modal-list-item">
                         <h3 class="artist-details-modal-title">Sex</h3>
                         <p class="artist-details-modal-text">${strGender}</p>
                     </li>
-                    ` : ''}
-                ${intMembers ? `
+                    `
+                    : ''
+                }
+                ${
+                  intMembers
+                    ? `
                     <li class="artist-details-modal-list-item">
                         <h3 class="artist-details-modal-title">Members</h3>
                         <p class="artist-details-modal-text">${intMembers}</p>
                     </li>
-                    ` : ''}
+                    `
+                    : ''
+                }
                 <li class="artist-details-modal-list-item">
                     <h3 class="artist-details-modal-title">Country</h3>
                     <p class="artist-details-modal-text">${strCountry}</p>
@@ -129,19 +139,28 @@ async function showArtistDetails(artistId) {
                 </ul>
                 
                 ${
-                    Array.isArray(genres) && genres.length
-                      ? `<ul class="artist-details-modal-block-genres">${genres
-                          .map(g => `<li class="artist-details-modal-block-genres-item">${g}</li>`)
-                          .join('')}</ul>`
-                      : ''
+                  Array.isArray(genres) && genres.length
+                    ? `<ul class="artist-details-modal-block-genres">${genres
+                        .map(
+                          g =>
+                            `<li class="artist-details-modal-block-genres-item">${g}</li>`
+                        )
+                        .join('')}</ul>`
+                    : ''
                 }
             </div>
             </div>
             <h3 class="artist-details-modal-albums">Albums</h3>
             <div class="artist-details-modal-albums-wrap">
-            ${albums.length > 0 ? albums.map(album => `
+            ${
+              albums.length > 0
+                ? albums
+                    .map(
+                      album => `
                 <div class="artist-details-modal-albums-list">
-                <h4 class="artist-details-modal-albums-list-title">${album.strAlbum}</h4>
+                <h4 class="artist-details-modal-albums-list-title">${
+                  album.strAlbum
+                }</h4>
                 <table>
                 <thead class="artist-details-modal-albums-list-table-head">
                     <tr>
@@ -151,50 +170,67 @@ async function showArtistDetails(artistId) {
                     </tr>
                 </thead>
                 <tbody class="artist-details-modal-albums-list-table-body">
-                    ${album.tracks?.map(track => `
+                    ${
+                      album.tracks
+                        ?.map(
+                          track => `
                     <tr>
-                        <td class="artist-details-modal-albums-list-table-text col-1">${track.strTrack}</td>
-                        <td class="artist-details-modal-albums-list-table-text col-2">${formatDuration(track.intDuration)}</td>
+                        <td class="artist-details-modal-albums-list-table-text col-1">${
+                          track.strTrack
+                        }</td>
+                        <td class="artist-details-modal-albums-list-table-text col-2">${formatDuration(
+                          track.intDuration
+                        )}</td>
                         <td class= "col-3">
-                            ${track.movie && track.movie !== 'null' ? `
+                            ${
+                              track.movie && track.movie !== 'null'
+                                ? `
                                 <a class="modal-link-youtube" href="${track.movie}" target="_blank" aria-label="Watch a video on YouTube">
                                     <svg class="modal-youtube" width="24" height="20">
                                         <use href="${sprite}#icon-youtube"></use>
                                     </svg>
                                 </a>
-                            ` : ''}
+                            `
+                                : ''
+                            }
                         </td>
                     </tr>
-                    `).join('') || ''}
+                    `
+                        )
+                        .join('') || ''
+                    }
                 </tbody>
                 </table>
             </div>
-            `).join('') : '<p>No albums found</p>'}
+            `
+                    )
+                    .join('')
+                : '<p>No albums found</p>'
+            }
             </div>
         </div>
         `;
 
+    refs.artistDetailsModal.innerHTML = `${markup}`;
 
-        refs.artistDetailsModal.innerHTML = `${markup}`;
-
-        const closeBtn = refs.artistDetailsModalBackdrope.querySelector('.artist-details-modal-close-btn');
-        closeBtn.addEventListener('click', closeModal);
-  
-
-    } catch (err) {
-        toastError(err.message);
-    } finally {
-        hideLoader();
-    }
+    const closeBtn = refs.artistDetailsModalBackdrope.querySelector(
+      '.artist-details-modal-close-btn'
+    );
+    closeBtn.addEventListener('click', closeModal);
+  } catch (err) {
+    toastError(err.message);
+  } finally {
+    hideLoader();
+  }
 }
 
-
 refs.artistsList.addEventListener('click', e => {
-    const btn = e.target.closest('.artist-cta');
-    if(!btn) return;
-    
-    const artistId = btn.dataset.artistId;
-    if(artistId) {
+  const card = e.target.closest('.artist-card');
+  if (!card) return;
+
+  const btn = card.querySelector('.artist-cta');
+  const artistId = btn.dataset.artistId;
+  if (artistId) {
     showArtistDetails(artistId);
-    }
+  }
 });
